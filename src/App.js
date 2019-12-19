3fe45df0af9a2631e8a7a0e38103ea765cbc3fbe
tyapama <tyapama@gmail.com>
2019-12-19 23:19:54 +0900

    React.Fragmentやjsxないのjavascript記入方法を学習

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+//jsxを使用する場合はReactは必要
 import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-//    return React.createElement(
-//        "h1",
-//        null,
-//        "hello,world!!"
-//      );
     return (
-      <h1>hello,world!</h1>
+      // React.Fragmentで余計なタグを出力せずに済む
+      <React.Fragment>
+        {/* htmlForはラベルとフォームパーツの関連づけ */}
+        <label htmlFor="name">名前</label>
+        <input id="name" type="text" onChange={(event)=>{console.log(event.target.value)}}/>
+      </React.Fragment>
     );
   }
 }
